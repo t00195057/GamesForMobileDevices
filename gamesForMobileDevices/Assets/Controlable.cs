de76df631dbdf244b45cf26ab1a4ac8c740045d7
t00195057 <jpjohara@gmail.com>
2020-02-21 17:52:47 +0000

    submit 1

---
+++ gamesForMobileDevices/Assets/Controlable.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlable : MonoBehaviour
+{ Vector3 des_Destionation;
+    Renderer my_renderer;
+    Controlable my_obj;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        des_Destionation = transform.position;
+        my_renderer = GetComponent<Renderer>();
+        my_obj = GetComponent<Controlable>();
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, des_Destionation, 0.2f);
+    }
+
+    internal void move_up()
+    {
+        my_renderer.material.color = Color.yellow;
+
+    }
+
+    internal void dragObject(Ray r, Vector3 cameraPos)
+    {
+        var objectDistance = Vector3.Distance(my_obj.transform.position, cameraPos);
+
+        my_obj.newDragPosition(r.GetPoint(objectDistance));
+
+    }
+    internal void scaleObject()
+    {
+        my_renderer.material.color = Color.yellow;
+
+    }
+    internal void rotateObject()
+    {
+        my_renderer.material.color = Color.yellow;
+
+    }
+  
+    internal void newDragPosition(Vector3 vector)
+    {
+        des_Destionation = vector;
+    
+    }
+    internal void go_red()
+    {
+        my_renderer.material.color = Color.red;
+    }
+
+    internal void go_white()
+    {
+        my_renderer.material.color = Color.white;
+    }
+
+ 
+
+    internal void select(Controlable tempObJ)
+    {
+        go_red();
+        my_obj = tempObJ;
+    }
+
+    internal void deselect()
+    {
+     go_white();
+        my_obj = null;
+    }
+}
---
+++ gamesForMobileDevices/Assets/TouchControl.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchControl : MonoBehaviour
+{
+    Controlable my_obj;
+    bool mouceDown = false;
+    public Camera my_camera = new Camera();
+    float countdown = 0.3f;
+    bool tapped = false;
+    bool tapIsUp = false;
+    Plane plane;
+    float initialcam_angle;
+    float initialobj_angle;
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+    float initial_rotation;
+    float cameraSpeed = 0.01f;
+    Touch previousTouch2;
+    void Start()
+    {
+        my_camera = Camera.main; 
+    }
+
+    // Update is called once per frame
+    float timeLeft = 2;
+    void Update()
+    {
+        
+        print(Input.acceleration);
+        if (Input.touchCount >0)
+        {
+      if(tapped)
+        {
+            countdown -= Time.deltaTime;
+         
+        }
+               
+                if (Input.touchCount == 2)
+                {
+                    Touch touch1 = Input.GetTouch(0);
+                    Touch touch2 = Input.GetTouch(1);
+                   
+                    Vector2 touchOnePrevPos = touch1.position - touch1.deltaPosition;
+                    Vector2 touchTwoPrevPos = touch2.position - touch2.deltaPosition;                     
+                    float prevTouchDeltaMag = (touchOnePrevPos - touchTwoPrevPos).magnitude;
+                    float touchDeltaMag = (touch1.position - touch2.position).magnitude;
+                    float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+                    float pinchAmount = deltaMagnitudeDiff * 0.2f * Time.deltaTime;
+
+                    if (deltaMagnitudeDiff > 2 || deltaMagnitudeDiff < -2)
+                    {
+                        if (my_obj)
+                        {
+                            my_obj.transform.localScale -= new Vector3(pinchAmount, pinchAmount, pinchAmount);
+                       
+                        }
+                        if (!my_obj)
+                        {
+                            Vector3 focalPoint = Vector3.zero;
+                            Vector3 direction = my_camera.transform.position - focalPoint;
+                            float newDistance = direction.magnitude / deltaMagnitudeDiff;
+                            my_camera.transform.position = newDistance * direction.normalized;
+                   
+                        }
+                    }
+               
+                    else
+                    {  
+                        Vector2 diff = touch2.position - touch1.position;
+            
+                    
+                  
+
+                    var angle = Mathf.Rad2Deg * (Mathf.Atan2(diff.y, diff.x));
+                         
+                             previousTouch2 = touch2;
+                    if (my_obj)
+                        {
+                         var initialobj_orie= my_obj.transform.rotation;
+                        my_obj.transform.rotation = initialobj_orie*Quaternion.AngleAxis(angle - initialobj_angle, my_obj.transform.forward);
+                        initialobj_angle = angle;
+                    }
+                        else
+                        {
+                             var initialcam_orie= my_camera.transform.rotation;
+                            my_camera.transform.rotation = initialcam_orie*Quaternion.AngleAxis(angle- initialcam_angle, my_camera.transform.forward);
+                        initialcam_angle = angle;
+                        }
+                   
+                    }
+                }
+            
+
+
+        
+      
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                tapped = true;
+               
+                tapIsUp = false;
+            
+
+            }
+           
+                if (Input.touchCount == 1)
+                {
+                    Touch current_touch = Input.GetTouch(0);
+                    Ray my_ray = Camera.main.ScreenPointToRay(current_touch.position);
+                    RaycastHit info;
+
+                if (countdown > 0)
+                    {
+                         if (Physics.Raycast(my_ray, out info))
+                         {
+                            Controlable object_hit = info.transform.GetComponent<Controlable>();
+
+                            if (object_hit)
+                            {
+                                if (my_obj)
+                                {
+                                    my_obj.deselect();
+                                     my_obj = null;
+                                }
+                                object_hit.select(object_hit);
+                                my_obj = object_hit;
+                            }
+
+                        else
+                        {
+                            if (my_obj)
+                            {
+                                my_obj.deselect();
+                                my_obj = null;
+                            }
+                        }
+                    }
+                        else
+                        {
+                            if (my_obj)
+                            {
+                                my_obj.deselect();
+                                my_obj = null;
+                        }
+
+                        }
+                }
+                
+                else
+                {
+                    if (my_obj)
+                    {
+                        Vector3 cameraPos = my_camera.transform.position;
+                        my_obj.dragObject(my_ray, cameraPos);
+                    }
+
+                    if (!my_obj)
+                    {
+                        my_camera.transform.position += cameraSpeed * my_camera.transform.right * current_touch.deltaPosition.x + cameraSpeed * my_camera.transform.up * current_touch.deltaPosition.y;
+
+                    }
+                }
+            }
+                if(Input.GetTouch(0).phase== TouchPhase.Ended)
+                { 
+                tapped = false;
+                countdown = 0.3f;
+                }
+        }
+    }
+
+}
